a965a84c72121b3ec811b1cc48e624344160c8b6
pchan <pichangetheworld@gmail.com>
2014-06-22 14:52:15 -0400

    adding signin activity

---
+++ src/com/gpsocial/SigninActivity.java
@@ -0,0 +1,19 @@
+package com.gpsocial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SigninActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signin);
+	}
+	
+	public void TwitterLogin(View view) {
+		startActivity(new Intent(this, MainActivity.class));
+	}
+}
